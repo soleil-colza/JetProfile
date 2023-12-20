f1ffdd238d55c6abf4e99ae44399d56e3bc5acee
Hina Nakahira <soleil.colza@icloud.com>
2023-12-21 01:11:54 +0900

    Added Accompanist and changed the StatusBarColor to white

--- app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -35,6 +37,10 @@ fun JetProfileTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         LightColorPalette
     }
 
+    rememberSystemUiController().setStatusBarColor(
+        color = Color.White,
+        darkIcons = true,
+    )
     MaterialTheme(
         colors = colors,
         typography = Typography,
